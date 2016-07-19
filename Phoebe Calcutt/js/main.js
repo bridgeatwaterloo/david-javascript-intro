46fab3063d18dfa49373626c51f786f4929f975b
CalcuttP <Phoebe@Phoebes-MacBook-Air.local>
2016-07-19 14:22:15 +0100

    David-App
    David-App

--- Phoebe Calcutt/js/main.js
+++ Phoebe Calcutt/js/main.js
@@ -1,1 +1,16 @@
-// Our JavaScript goes in here
+// Commented out code because I do not want it popping up after every refresh 
+// var username = prompt ("what is your name?")
+// alert ("Hi there" + username );
+
+
+
+// Target all elements with a class of "view" using jQuery: $('view') 
+// Then run the .hide() method http://api.jquery.com/hide // 
+$('.view').hide(); 
+
+//TASK: Target .view-checkin and show it 
+
+$('.view-checkin').show();
+
+
+
