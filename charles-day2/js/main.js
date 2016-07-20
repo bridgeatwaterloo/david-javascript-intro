795b6ff63a01e30cf6237b8ad20f37edc66a7a7b
Charles Dockerill <Charles.dockerill@gmail.com>
2016-07-20 10:56:32 +0100

    Button Yes/no
    Alert box on press

--- charles-day2/js/main.js
+++ charles-day2/js/main.js
@@ -1,1 +1,14 @@
-// Our JavaScript goes in here
+// Our JavaScri$(pt goes in here
+
+$('.view-checkin').hide();
+
+$('.btn-success').click(Yes);
+	function Yes(){
+		alert("Clicked yes");
+		showView('.view-default');
+	}
+
+$('.btn-danger').click(No);
+	function No(){
+		alert("Clicked no");
+	}
